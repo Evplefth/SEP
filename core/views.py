f33a547db34321af16476c8a2fd65f18296096fe
Evplefth <ev.plefth@gmail.com>
2026-03-12 12:52:34 +0200

    Initial commit

---
+++ core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+
+@login_required
+def home(request):
+    return render(request, 'core/home.html')
+
+
+@login_required
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'core/user_list.html', {'users': users})\
+    
+
